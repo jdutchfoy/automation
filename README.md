bcdf04ccfe5e08278e9daca1d242fa7255b4df68
jdutchfoy <jdutchfoy@gmail.com>
2023-07-13 13:47:52 -0700

    project functions double checking all files tracked

--- README.md
+++ README.md
@@ -1,9 +1,33 @@
-# README.md
+# README.md 
 
-## Automation Project
+```markdown
+# Automation Project
 
-Author: Dutch Foy
+This project is a Python automation script that helps with various tasks such as folder management, document sorting, log file parsing, and menu-driven application functionality.
 
-- Designed to streamline and automate several tasks, including folder management, document sorting, log file parsing, and a menu-driven application.
+- The `automation-project` directory contains the main script `main.py` responsible for the automation tasks.
+- The `tests` directory contains unit tests in `test_automation_project.py` to ensure the functionality of the script.
 
-## Requirements
+## Getting Started
+
+To use the automation script, follow these steps:
+
+1. Clone or download the project to your local machine.
+2. Make sure you have Python 3.x installed.
+3. Install the required dependencies by running `pip install -r requirements.txt` in the project root directory.
+
+## Running the Script
+
+To run the automation script, navigate to the `automation-project` directory and execute the following command:
+
+```
+python main.py
+```
+
+## Running Tests
+
+The project includes unit tests to verify the functionality of the automation script. To run the tests, navigate to the `tests` directory and execute the following command:
+
+```
+python -m unittest test_automation_project.py
+```
--- automation-project/test_automation_project.py
+++ automation-project/test_automation_project.py
@@ -1,34 +1,51 @@
 import unittest
 import os
 import shutil
-from main import create_folder, handle_deleted_user, sort_documents, parse_log_file
+from main import create_folder, handle_deleted_user, sort_documents, parse_log_file, main_menu
 
 class TestAutomationProject(unittest.TestCase):
 
     def setUp(self):
+        # Set up a temporary folder for testing
         self.temp_folder = "temp_test_folder"
         os.mkdir(self.temp_folder)
 
     def tearDown(self):
+        # Clean up the temporary folder after testing
         shutil.rmtree(self.temp_folder)
 
     def test_create_folder(self):
+        # Test the create_folder function
+
+        # Prepare the necessary data
         folder_name = "NewFolder"
+
+        # Call the function to be tested
         create_folder(folder_name)
-        self.assertTrue(os.path.exists(folder_name))
+
+        # Check the expected outcome
+        self.assertTrue(os.path.exists(folder_name))  # Assert that the folder has been created
 
     def test_handle_deleted_user(self):
+        # Test the handle_deleted_user function
+
+        # Prepare the necessary data
         deleted_user = "user2"
         user_folder = os.path.join(self.temp_folder, deleted_user)
         temp_user_folder = os.path.join(self.temp_folder, "temp_user_folder")
         os.mkdir(user_folder)
 
+        # Call the function to be tested
         handle_deleted_user(deleted_user, user_folder, temp_user_folder)
 
-        self.assertFalse(os.path.exists(user_folder))
-        self.assertTrue(os.path.exists(temp_user_folder))
+        # Check the expected outcome
+        self.assertFalse(os.path.exists(user_folder))  # Assert that the user folder has been deleted
+        self.assertTrue(os.path.exists(temp_user_folder))  # Assert that the documents have been moved to the temporary folder
 
     def test_sort_documents(self):
+        # Test the sort_documents function
+
+        # Prepare the necessary data
         source_folder = "test_documents"
         os.mkdir(source_folder)
         test_files = ["document1.txt", "document2.pdf", "document3.docx"]
@@ -36,8 +53,33 @@ class TestAutomationProject(unittest.TestCase):
             with open(os.path.join(source_folder, file), "w"):
                 pass
 
+        # Call the function to be tested
         sort_documents(source_folder)
 
-        self.assertTrue(os.path.exists(os.path.join(source_folder, "text_files")))
-        self.assertTrue(os.path.exists(os.path.join(source_folder, "pdf_files")))
-        self.assertTrue(os.path.exists(os.path.join(source_folder, "word_files")))
+        # Check the expected outcome
+        self.assertTrue(os.path.exists(os.path.join(source_folder, "text_files")))  # Assert that the text files have been sorted correctly
+        self.assertTrue(os.path.exists(os.path.join(source_folder, "pdf_files")))  # Assert that the PDF files have been sorted correctly
+        self.assertTrue(os.path.exists(os.path.join(source_folder, "word_files")))  # Assert that the Word files have been sorted correctly
+
+    def test_parse_log_file(self):
+        # Test the parse_log_file function
+
+        # Prepare the necessary data
+        log_file = "test_log.log"
+        test_log_data = "This is an ERROR message.\nThis is a WARNING message.\n"
+        with open(log_file, "w") as file:
+            file.write(test_log_data)
+
+        # Call the function to be tested
+        parse_log_file(log_file)
+
+        # Check the expected outcome
+        self.assertTrue(os.path.exists("errors.log"))  # Assert that the errors log file has been created
+        self.assertTrue(os.path.exists("warnings.log"))  # Assert that the warnings log file has been created
+
+    def test_main_menu(self):
+        # TODO: Implement tests for the main_menu function
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
